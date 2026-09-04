26f7a6f67990a52bb8426b0d2f968b28086d5cb6
zmohammmad5-boop <zmohammmad5@gmail.com>
2026-09-03 20:15:18 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,16 @@
-const CACHE_NAME = 'wallet-pwa-v6';
-const CORE_ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './app-icon-192.png',
-  './app-icon-512.png',
-  './apple-touch-icon.png'
-];
-
+const VERSION='ghost-wallet-phone-same-ui-v1';
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
-  );
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
-
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
+  if(event.request.method === 'GET'){
+    event.respondWith(fetch(event.request));
+  }
 });
